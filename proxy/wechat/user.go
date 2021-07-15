5509766c8752e4fbadde5cbdaaba9f349c88c5dc
Nico <ainililia@163.com>
2021-07-15 20:29:58 +0800

    企业微信新增获取部门成员列表接口

--- proxy/wechat/user.go
+++ proxy/wechat/user.go
@@ -4,76 +4,62 @@ import (
 	"gitea.bjx.cloud/allstar/saturn/model/context"
 	"gitea.bjx.cloud/allstar/saturn/model/req"
 	"gitea.bjx.cloud/allstar/saturn/model/resp"
+	"gitea.bjx.cloud/allstar/saturn/util/json"
+	"github.com/LLLjjjjjj/work-wechat"
 	"github.com/polaris-team/dingtalk-sdk-golang/sdk"
 	"log"
 	"strconv"
 )
 
-func (d *dingProxy) GetUsers(ctx *context.Context, r req.GetUsersReq) resp.GetUsersResp {
-	client := &sdk.DingTalkClient{
-		AccessToken: ctx.TenantAccessToken,
-		AgentId:     d.AgentId,
-	}
-	deptIds := make([]string, 0)
+func (w *wechatProxy) GetUsers(ctx *context.Context, r req.GetUsersReq) resp.GetUsersResp {
 	if r.DepartmentID == "" {
-		deptIds = append(deptIds, "1")
-	} else {
-		deptIds = append(deptIds, r.DepartmentID)
+		r.DepartmentID = "0"
 	}
+	fetchChild := 0
 	if r.FetchChild {
-		allDeptIdsResp := d.GetDeptIds(ctx, req.GetDeptIdsReq{
-			ParentId:   deptIds[0],
-			FetchChild: r.FetchChild,
-		})
-		if !allDeptIdsResp.Suc {
-			return resp.GetUsersResp{Resp: allDeptIdsResp.Resp}
-		}
-		deptIds = append(deptIds, allDeptIdsResp.Data...)
+		fetchChild = 1
 	}
-	limit := 9999999
-	if r.Limit > 0 {
-		limit = r.Limit
+	action := work.GetDeptMemberList(ctx.TenantAccessToken, r.DepartmentID, fetchChild)
+	respBody, err := action.GetRequestBody()
+	if err != nil {
+		return resp.GetUsersResp{Resp: resp.ErrResp(err)}
 	}
-	userList := make([]resp.User, 0)
-	userContains := map[string]bool{}
-	for _, deptIdStr := range deptIds {
-		hasMore := true
-		var cursor int64 = 0
-		deptId, _ := strconv.ParseInt(deptIdStr, 10, 64)
-		for hasMore && (limit > len(userList)) {
-			deptUserListResp, err := client.GetDeptUserListV2(deptId, cursor, 100, "", nil, "")
-			if err != nil {
-				log.Println(err)
-				break
-			}
-			if deptUserListResp.ErrCode != 0 {
-				log.Println(deptUserListResp.ErrCode, deptUserListResp.ErrMsg)
-				break
-			}
-			cursor = deptUserListResp.Result.NextCursor
-			hasMore = deptUserListResp.Result.HasMore
+	deptMemberListResp := work.GetDeptMemberListResp{}
+	json.FromJsonIgnoreError(string(respBody), &deptMemberListResp)
 
-			respUserList := convertUsers(deptUserListResp.Result.List)
-			for _, respUser := range respUserList {
-				if !userContains[respUser.OpenID] {
-					userList = append(userList, respUser)
-					userContains[respUser.OpenID] = true
-				}
-			}
+	respUsers := make([]resp.User, 0)
+	for _, deptMember := range deptMemberListResp.UserList {
+		deptIdList := make([]string, 0)
+		for _, deptId := range deptMember.Department {
+			deptIdList = append(deptIdList, strconv.Itoa(deptId))
 		}
-		if limit <= len(userList) {
-			break
-		}
+		respUsers = append(respUsers, resp.User{
+			OpenID:  deptMember.OpenUserid,
+			UserID:  deptMember.Userid,
+			UnionID: deptMember.OpenUserid,
+			Name:    deptMember.Name,
+			EnName:  deptMember.Name,
+			Email:   deptMember.Email,
+			Mobile:  deptMember.Mobile,
+			IsAdmin: false,
+			Avatar: resp.Avatar{
+				Avatar72:     deptMember.Avatar,
+				Avatar240:    deptMember.Avatar,
+				Avatar640:    deptMember.Avatar,
+				AvatarOrigin: deptMember.Avatar,
+			},
+			DepartmentIds: deptIdList,
+		})
 	}
 	return resp.GetUsersResp{
 		Resp: resp.SucResp(),
 		Data: resp.GetUsersRespData{
-			Users: userList,
+			Users: respUsers,
 		},
 	}
 }
 
-func (d *dingProxy) GetUser(ctx *context.Context, id string) resp.GetUserResp {
+func (w *wechatProxy) GetUser(ctx *context.Context, id string) resp.GetUserResp {
 	client := &sdk.DingTalkClient{
 		AccessToken: ctx.TenantAccessToken,
 		AgentId:     d.AgentId,
@@ -108,32 +94,4 @@ func (d *dingProxy) GetUser(ctx *context.Context, id string) resp.GetUserResp {
 			DepartmentIds: deptIdList,
 		},
 	}
-}
-
-func convertUsers(users []sdk.UserDetailInfoV2) []resp.User {
-	respUsers := make([]resp.User, len(users))
-	for i, user := range users {
-		deptIdList := make([]string, 0)
-		for _, deptId := range user.DeptIdList {
-			deptIdList = append(deptIdList, strconv.FormatInt(deptId, 10))
-		}
-		respUsers[i] = resp.User{
-			OpenID:  user.UnionID,
-			UserID:  user.UserID,
-			UnionID: user.UnionID,
-			Name:    user.Name,
-			EnName:  user.Name,
-			Email:   user.Email,
-			Mobile:  user.Mobile,
-			IsAdmin: user.Admin,
-			Avatar: resp.Avatar{
-				Avatar72:     user.Avatar,
-				Avatar240:    user.Avatar,
-				Avatar640:    user.Avatar,
-				AvatarOrigin: user.Avatar,
-			},
-			DepartmentIds: deptIdList,
-		}
-	}
-	return respUsers
 }
